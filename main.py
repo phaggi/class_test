0583dafe19ebff960f499115a25ca74e12f2f52a
phaggi <phaggi@gmail.com>
2022-02-12 23:40:27 +0300

    make class test

--- main.py
+++ main.py
@@ -4,10 +4,9 @@
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
 
+
+test
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
